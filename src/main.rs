f4098e0926398b113ba3bdd56cf99d8e56fa747b
us3r64bit <arth.prajapati@outlook.com>
2024-08-21 18:35:14 +0530

    print file content

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    pattern: String,
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    let content = std::fs::read_to_string(&args.path);
+    match content {
+        Ok(data) => {
+            println!("file content {}", data);
+        }
+        Err(err) => {
+            println!("Error, {}", err);
+        }
+    }
 }
