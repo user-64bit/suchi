c08ca12b19c3003e1ececa861366c4a406e34a5e
user-64bit <arth.prajapati@outlook.com>
2024-10-08 15:01:31 +0530

    init method

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,44 @@
 use std::env;
+use std::fmt::Error;
+use std::fs::OpenOptions;
 
 fn main() {
-  let args: Vec<String> = env::args().collect();
+    // initialization of .suchi
+    match init() {
+        Ok(_) => println!(".suchi file created at $HOME"),
+        Err(e) => println!("Error occured: {}", e),
+    };
 
-  if args.len() > 1 {
-    let command = &args[1];
-    match &command[..] {
-      "show" => println!("show todos, {:?}", &args[2..]),
-      "add" => println!("add todo, {:?}", &args[2..]),
-      "done" => println!("done todo, {:?}", &args[2..]),
-      "delete" => println!("delete todo, {:?}", &args[2..]),
-      "edit" => println!("edit todo, {:?}", &args[2..]),
-      "clear" => println!("clear todos, {:?}", &args[2..]),
-      "--help" | "help" | "-h" | _ => help(),
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() > 1 {
+        let command = &args[1];
+        match &command[..] {
+            "show" => println!("show todos, {:?}", &args[2..]),
+            "add" => println!("add todo, {:?}", &args[2..]),
+            "done" => println!("done todo, {:?}", &args[2..]),
+            "delete" => println!("delete todo, {:?}", &args[2..]),
+            "edit" => println!("edit todo, {:?}", &args[2..]),
+            "clear" => println!("clear todos, {:?}", &args[2..]),
+            "--help" | "help" | "-h" | _ => help(),
+        }
+    } else {
+        help();
     }
-  } else {
-    help();
-  }
 }
 
+fn init() -> Result<(), Error> {
+    let home: String = env::var("HOME").expect("HOME directory not found");
+    let suchi_path = format!("{home}/.suchi");
+	// if .suchi doesn't exits then create.
+    let _file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .append(true)
+        .create(true)
+        .open(suchi_path);
+    Ok(())
+}
 
 const HELP: &str = r#"
 Usage: suchi [COMMAND] [OPTIONS]
