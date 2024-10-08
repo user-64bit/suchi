a74c7f14064a2de1ab7a507f24d61a6cff545ecf
user-64bit <arth.prajapati@outlook.com>
2024-10-08 11:46:28 +0530

    commands

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
-use std::io;
+use std::env;
 
 fn main() {
-    println!("Enter your name :");
-    let mut name = String::new();
-    io::stdin().read_line(&mut name).expect("Failed to read line");
+  let args: Vec<String> = env::args().collect();
 
-    println!("Hello {}", name);
+  if args.len() > 1 {
+    let command = &args[1];
+    match &command[..] {
+      "show" => println!("show todos, {:?}", &args[2..]),
+      "add" => println!("add todo, {:?}", &args[2..]),
+      "done" => println!("done todo, {:?}", &args[2..]),
+      "delete" => println!("delete todo, {:?}", &args[2..]),
+      "edit" => println!("edit todo, {:?}", &args[2..]),
+      "clear" => println!("clear todos, {:?}", &args[2..]),
+      _ => println!("get some help"),
+    }
+  } else {
+    println!("--help to know more...")
+  }
 }
